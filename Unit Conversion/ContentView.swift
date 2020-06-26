69bc0271db1ce8bd8d07271adb9a20a48c6e9e3f
Nate Lee <nateleee.dev@gmail.com>
2020-06-27 00:09:20 +0800

    Added another section of the form, which is a source unit picker.

--- Unit Conversion/ContentView.swift
+++ Unit Conversion/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input = ""
+    @State private var sourceSelected = 0
+    @State private var dstSelected = 0
+    
+    private var sourceUnits = ["HP", "Brake HP", "Kilowatt"]
+    private var dstUnits = ["HP", "Brake HP", "Kilowatt"]
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Input here:")) {
                     TextField("", text: $input).keyboardType(.decimalPad)
+                }
+                Section(header: Text("Source Unit")) {
+                    Picker("Select Source Unit", selection: $sourceSelected) {
+                        ForEach(0..<sourceUnits.count) {
+                            Text("\(self.sourceUnits[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
                 }
             }.navigationBarTitle("Unit Conversion")
         }
